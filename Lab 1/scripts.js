85ba0fa0341ce18c42d917a20ec04a785338248f
Jessica Maltby <P2647548@my365.dmu.ac.uk>
2024-01-30 16:18:24 +0000

    Lab 2

--- Lab 1/scripts.js
+++ Lab 1/scripts.js
@@ -1,2 +1,23 @@
-console.log("Hello!");
-myElement.textContent = "I've changed the content!";
+
+
+var ishighlighted = false;
+
+function highlight(){
+    var section1 = document.getElementById("fimage")
+    var section2 = document.getElementById("f")
+    var currentColor1 = section1.style.borderColor;
+    var currentColor2 = section2.style.backgroundColor.tos;
+    console.log(currentColor1);
+    if(!ishighlighted){
+        section1.style.borderColor = "#ffffff"
+        section2.style.backgroundColor = "#ffffff";
+        
+        
+    } else{
+        section1.style.borderColor = "#deb887";
+        section2.style.backgroundColor = "#ffe4c4";
+        
+    }
+    ishighlighted = !ishighlighted;
+}
+
